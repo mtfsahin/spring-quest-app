4c588ea708b177d20698c46cebf7677f1889aff8
mustafa sahin <mustafasahin@Mustafas-MBP.home>
2023-07-18 20:52:08 +0300

    create spring & install dependencies & jdbc connection

---
+++ backend/src/main/java/com/example/Quest/App/Spring/QuestAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Quest.App.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestAppSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestAppSpringApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/Quest/App/Spring/QuestAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Quest.App.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestAppSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
