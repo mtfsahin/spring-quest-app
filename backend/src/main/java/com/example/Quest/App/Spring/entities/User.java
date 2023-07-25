485e3486042ceeb83503b7977a3eaa37367e223c
mustafa sahin <mustafasahin@Mustafas-MBP.home>
2023-07-26 00:10:59 +0300

    update: changed structure and use service structure

--- backend/src/main/java/com/example/Quest/App/Spring/entities/User.java
+++ backend/src/main/java/com/example/Quest/App/Spring/entities/User.java
@@ -1,19 +1,51 @@
-package com.example.Quest.App.Spring;
+package com.example.Quest.App.Spring.entities;
 
 import jakarta.persistence.Entity;
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+
+
 
 @Entity
 @Table(name = "user")
-@Data 
+
 
 public class User {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 	String userName;
 	String password;
-	
+    public User() {
+    }
+
+    // Getter ve Setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
